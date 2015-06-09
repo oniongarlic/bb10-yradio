2ddc62a2e490ca4004cf1c999ada2a580ac93b58
Kaj-Michael Lang <milang@tal.org>
2015-06-09 10:48:19 +0300

    Finnish translation

---
+++ translations/bb10_yradio_fi.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fi">
+<context>
+    <name>AboutSheet</name>
+    <message>
+        <source>About - Y-Radio</source>
+        <translation>Ohjelmasta Y-Radio</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Sulje</translation>
+    </message>
+    <message>
+        <source>Unofficial Radio application for Finnish broadcasting company Yleisradio radio stations.</source>
+        <translation>Epävirallinen YLE Radion kuunteluohjelma</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>About</source>
+        <translation>Ohjelmasta</translation>
+    </message>
+    <message>
+        <source>Web site</source>
+        <translation>Kanavan kotisivulle</translation>
+    </message>
+    <message>
+        <source>Follow on Twitter</source>
+        <translation>Seuraa Twitteri:ssä</translation>
+    </message>
+    <message>
+        <source>Follow on Facebook</source>
+        <translation>Seuraa Facebooki:ssa</translation>
+    </message>
+    <message>
+        <source>Follow on YouTube</source>
+        <translation>Seuraa YouTube:ssa</translation>
+    </message>
+</context>
+</TS>
